a2ade9be9d9523b82629b39b8894f5832b60b053
Farl Lee <spring_snow@hotmail.com>
2024-06-16 14:52:36 +0800

    Try PWA

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Prototyper-More Sliding Puzzle-0.1.0";
+const contentToCache = [
+    "Build/sliding-puzzle.loader.js",
+    "Build/sliding-puzzle.framework.js",
+    "Build/sliding-puzzle.data",
+    "Build/sliding-puzzle.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
